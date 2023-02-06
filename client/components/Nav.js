498d92105ff70d53c2fcc27fb82cb339c65e1da8
Maciel Júnior <macielfcjunior@icloud.com>
2023-02-06 11:19:00 -0300

    feat: :lock: initial commit

---
+++ client/components/Nav.js
@@ -0,0 +1,108 @@
+import { useContext, useEffect, useState } from "react";
+import Link from "next/link";
+import { UserContext } from "../context";
+import { useRouter } from "next/router";
+
+const Nav = () => {
+  const [current, setCurrent] = useState("");
+  const [state, setState] = useContext(UserContext);
+
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+  
+  const router = useRouter();
+  const logout = () => {  
+    window.localStorage.removeItem("auth");
+    setState(null);
+    router.push("/login");
+  };
+
+  return (
+    <nav className={"nav d-flex justify-content-end primary bg-primary"}
+      style={{backgroundColor: "blue"}}
+    >
+
+      <Link 
+        href="/" 
+        className={`nav-link logo ${current ==="/" && "active"}`} 
+        data-testid="home-link">
+          UnBook
+      </Link>
+
+
+      {state !== null ? (
+        <>
+          <div class="dropdown">
+            <a 
+              class="btn dropdown-toggle text_light" 
+              role="button"
+              id="dropdownMenuLink"
+              data-bs-toggle="dropdown" 
+              aria-expanded="false"
+            >
+              {state && state.user && state.user.name}
+            </a>
+
+            <ul class="dropdown-menu">
+              <li>
+                <Link 
+                  href="/user/dashboard" 
+                  className={`nav-link dropdown-item ${
+                    current === "/user/dashboard" && "active"
+                  }`}>
+                    Feed
+                </Link>
+              </li>
+              <li>
+                <Link 
+                  href="/user/profile/update" 
+                  className={`nav-link dropdown-item ${
+                    current === "/user/profile/update" && "active"
+                  }`}>
+                    Perfil
+                </Link>
+              </li>
+              <li>
+                <a
+                  onClick={logout}
+                  className="nav-link" 
+                  data-testid="logout-link">
+                    Sair
+               </a>
+              </li>
+            </ul>
+          </div>
+        </>
+
+      ) : (
+
+        <>
+          <Link 
+            href="/login" 
+            className={`nav-link text-light ${
+              current === "/login" && "active"
+            }`}
+            data-testid="login-link">
+              Login
+          </Link>
+
+          <Link 
+            href="/register" 
+            className={`nav-link text-light ${
+              current === "/register" && "active"
+            }`}
+            data-testid="register-link">
+              Cadastrar
+          </Link>
+        </>
+
+      )}
+
+
+    </nav>
+
+  );
+};
+
+export default Nav;
---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+      max: 64,
+    },
+    secret: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    about: {},
+    photo: String,
+    following: [{ type: Schema.ObjectId, ref: 'User' }],
+    followers: [{ type: Schema.ObjectId, ref: 'User' }],
+
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);
